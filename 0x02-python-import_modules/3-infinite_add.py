00574f3be5d01d4c2862f775840afd549a6960e9
iDamina <daminailiya@gmail.com>
2023-11-02 23:01:33 +0100

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
-	if len(sys.argv) <= 1:
-		return
+    if len(sys.argv) <= 1:
+        return
+
+
 result = 0
 for arg in sys.argv[1:]:
-	result += int(arg)
+    result += int(arg)
 print(f"{result}")
 if __name__ == "__main__":
-	main()
+    main()
