659f214cf0541b14282b0eef6328a1d31763cff4
iDamina <daminailiya@gmail.com>
2023-11-05 22:59:00 +0100

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for item in my_list:
-        if type(item) == int:
-            print("{}".format(item))
+        print("{}".format(item))
