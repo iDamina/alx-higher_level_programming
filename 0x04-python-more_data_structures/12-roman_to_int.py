46cc1132bcc759d121558d1adca7faf61fde95e1
iDamina <daminailiya@gmail.com>
2023-11-09 15:56:43 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or not roman_string:
+    if type(roman_string) is not str or roman_string == {}:
         return (0)
 
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
