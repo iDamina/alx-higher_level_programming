a06a4e5124d441a5331ac0ba9f230edda6d976f8
iDamina <daminailiya@gmail.com>
2023-11-29 22:53:59 +0100

    square 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,6 +17,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """ area of the square."""
+    def area(self):
+        """ area of the square."""
         return (self.__size ** 2)
