007464b367fe400d7ececa0e0ee4158f8ba48f6e
iDamina <daminailiya@gmail.com>
2023-12-05 23:39:45 +0100

    appending text to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines the append_write function."""
+
+
+def append_write(filename="", text=""):
+    """Appends filename with UTF8 text file formating."""
+    with open(filename, "a", encoding="utf-8") as f:
+            return f.write(text)
