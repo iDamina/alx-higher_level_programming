f5136f12be8338edcb1dfe95fec8db2bc94ba1f1
iDamina <daminailiya@gmail.com>
2023-12-15 01:33:18 +0100

    adding getter and setter for the square model

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,17 +14,17 @@ class Square(Rectangle):
         """ Returns string info about this square."""
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
-    """
+
     @property
     def size(self):
-        '''Size of this square.'''
+        """ Size of this square. """
         return self.width
 
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
-
+    """
     def __update(self, id=None, size=None, x=None, y=None):
         '''Internal method that updates instance attributes via */**args.'''
         if id is not None:
