dd175b74a59186a81f2b4dd7dd19de9416b55d0b
iDamina <daminailiya@gmail.com>
2023-12-15 01:07:27 +0100

    adding public method update(self, *args)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,20 +78,20 @@ class Rectangle(Base):
         return '[{}] ({}) {}/{} - {}/{}'.\
             format(type(self).__name__, self.id, self.x, self.y,
                    self.width, self.height)
-    """
+
     def __update(self, id=None, width=None, height=None, x=None, y=None):
-        '''Internal method that updates instance attributes via */**args.'''
+        """ Internal method that updates instance attributes via */**args. """
         if id is not None:
             self.id = id
-            if width is not None:
-                self.width = width
-                if height is not None:
-                    self.height = height
-                    if x is not None:
-                        self.x = x
-                        if y is not None:
-                            self.y = y
-
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+    """
 
     def update(self, *args, **kwargs):
         '''Updates instance attributes via no-keyword & keyword args.'''
