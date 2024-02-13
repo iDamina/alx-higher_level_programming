77410d6ee9bdec5de1de102dd8e09feda763b2c3
iDamina <daminailiya@gmail.com>
2024-02-13 16:11:22 +0100

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const numOfArguments = process.argv.length - 2;
 
 if (numOfArguments === 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (numOfArguments === 1) {
-	  console.log('Argument found');
+  console.log('Argument found');
 } else {
-	  console.log('Arguments found');
+  console.log('Arguments found');
 }
