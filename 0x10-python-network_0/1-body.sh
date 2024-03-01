726e28683eeaec628898f178b9f7fcd7dc90d3b5
iDamina <daminailiya@gmail.com>
2024-03-01 07:48:23 -0800

    1-body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request and display the body if the status code is 200
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1" || echo $response
+curl -Ls "$1"
