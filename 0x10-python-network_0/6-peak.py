59410c9e019a4b1224ee7ce758ae0f4ec27e9fb7
iDamina <daminailiya@gmail.com>
2024-03-03 16:32:15 -0800

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,16 +8,20 @@ def find_peak(list_of_integers):
     Returns:
     - The peak element
     """
-    if not list_of_integers:
+    list_int = list_of_integers
+    if not list_int:
         return None
+    length = len(list_int)
 
-    low, high = 0, len(list_of_integers) - 1
+    low, high = 0, length - 1
 
     while low < high:
-        mid = (low + high) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
+        mid = low + (high - low) // 2
+        if list_int[mid] > list_int[mid - 1] and list_int[mid] > list_int[mid + 1]:
+            return list_int[mid]
+        if list_int[mid - 1] > list_int[mid + 1]:
             high = mid
         else:
             low = mid + 1
 
-    return list_of_integers[low]
+    return list_int[low]
