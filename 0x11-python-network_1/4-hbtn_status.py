d767d00523b9fe155b988843c8083d20c40117d3
iDamina <daminailiya@gmail.com>
2024-03-11 10:51:36 +0100

    hbtn_status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,9 +4,9 @@ Python script that fetches https://intranet.hbtn.io/status
 """
 import requests
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    res = requests.get("https://intranet.hbtn.io/status")
+    res = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
