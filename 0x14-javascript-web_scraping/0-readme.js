3dd0d0aa4ab7e01fac4206fbe743ea5645c5568e
iDamina <daminailiya@gmail.com>
2024-03-27 00:52:22 +0100

    0 readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length !== 3) {
+  console.log('Usage: node script.js <file_path>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, content) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(content);
+  }
+});
