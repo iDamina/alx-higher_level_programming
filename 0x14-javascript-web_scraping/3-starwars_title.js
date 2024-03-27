1384975fb31206f68848b4a9fd6a9a5c86faeb0c
iDamina <daminailiya@gmail.com>
2024-03-27 01:23:01 +0100

    starwars title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.log('Usage: node script.js <movie_id>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
